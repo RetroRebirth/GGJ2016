1493fe117637806e869e17af560849c66285bf59
LeJon McGowan <lejonmcgowan@gmail.com>
2016-01-29 22:06:49 -0800

    works for sure: also web is working; see instructions

--- desktop/src/com/teamname/goaton/desktop/DesktopLauncher.java
+++ desktop/src/com/teamname/goaton/desktop/DesktopLauncher.java
@@ -1,8 +1,8 @@
-package com.mygdx.game.desktop;
+package com.teamname.goaton.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.MainGame;
+import com.teamname.goaton.MainGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
