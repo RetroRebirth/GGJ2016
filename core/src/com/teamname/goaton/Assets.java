17ae008cbfe4ab60394938c5390062f171e16297
Christopher Williams <chrsw49@gmail.com>
2016-01-30 14:26:35 -0800

    animated goat sprite renderer

--- core/src/com/teamname/goaton/Assets.java
+++ core/src/com/teamname/goaton/Assets.java
@@ -5,5 +5,17 @@ package com.teamname.goaton;
  */
 public class Assets {
     public final static String player = "art/Player.png";
-    public final static String goat = "art/Goat.png";
+    // Goat sprite assets
+    public final static String goat_D   = "art/goat_down.png";
+    public final static String goat_DL  = "art/goat_down-left.png";
+    public final static String goat_DR  = "art/goat_down-right.png";
+    public final static String goat_L   = "art/goat_left.png";
+    public final static String goat_LI  = "art/goat_left-in.png";
+    public final static String goat_LO  = "art/goat_left-out.png";
+    public final static String goat_R   = "art/goat_right.png";
+    public final static String goat_RI  = "art/goat_right-in.png";
+    public final static String goat_RO  = "art/goat_right-out.png";
+    public final static String goat_U   = "art/goat_up.png";
+    public final static String goat_UL  = "art/goat_up-left.png";
+    public final static String goat_UR  = "art/goat_up-right.png";
 }
