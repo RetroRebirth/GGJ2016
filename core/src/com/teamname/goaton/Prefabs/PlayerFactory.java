e700bcb6ca598417a1bd21154d8fbe14b87a5b2c
Christopher Williams <chrsw49@gmail.com>
2016-01-31 09:17:12 -0800

    player health component

--- core/src/com/teamname/goaton/Prefabs/PlayerFactory.java
+++ core/src/com/teamname/goaton/Prefabs/PlayerFactory.java
@@ -41,6 +41,7 @@ public class PlayerFactory extends GameObject {
         go.addComponent(new PlayerPickupComponent());
         go.addComponent(new GoatStackComponent());
         go.addComponent(new ParticleComponent("art/particleEffects/testExplode.p","explode"));
+        go.addComponent(new PlayerHealthComponent());
         go.tags.add("player");
         go.layer = Assets.ACTOR_LAYER;
         return go;
