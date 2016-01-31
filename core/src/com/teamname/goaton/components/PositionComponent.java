dbe8b35ced195fc1286203ef0ae41b9a5060ff51
Alex Ling <alex.ling.9495@gmail.com>
2016-01-31 08:08:46 +0000

    Spawn Areas work

--- core/src/com/teamname/goaton/components/PositionComponent.java
+++ core/src/com/teamname/goaton/components/PositionComponent.java
@@ -1,8 +1,12 @@
 package com.teamname.goaton.components;
 
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.teamname.goaton.Component;
 import com.teamname.goaton.GoatonWorld;
+
+import java.util.Arrays;
+import java.util.ArrayList;
 
 /**
  * Created by pya on 1/30/16.
@@ -13,12 +17,19 @@ public class PositionComponent extends Component {
 
     public PositionComponent() {}
 
+
     @Override
     protected void create() {
-        //final Component thisComp = this;
+        int numSpawns = GoatonWorld.SpawnAreas.size();
+        Rectangle chosen = GoatonWorld.SpawnAreas.get(GoatonWorld.Random.nextInt(numSpawns));
 
-        boundedWidth =  GoatonWorld.Random.nextFloat() * 100;
-        boundedHeight = GoatonWorld.Random.nextFloat() * 100;
+        float upBoundX = chosen.getX() + chosen.getWidth();
+        float upBoundY = chosen.getY() + chosen.getHeight();
+        float lowBoundX = chosen.getX();
+        float lowBoundY = chosen.getY();
+
+        boundedWidth =  GoatonWorld.Random.nextFloat() * (upBoundX - lowBoundX) + lowBoundX;
+        boundedHeight = GoatonWorld.Random.nextFloat() * (upBoundY - lowBoundY) + lowBoundY;
 
         gameObject.setPosition(new Vector2(boundedWidth, boundedHeight));
     }
